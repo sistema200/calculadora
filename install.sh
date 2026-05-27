31f53385ba79b30147ed43e29c20720b347f566c
sistema200 <sistemaoperativo610@gmail.com>
2026-05-27 16:49:44 -0400

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+#Dar permiso para que funcione
+sudo chmod +x calculadora
+#mover para inicial desde la terminal
+sudo mv calculadora /usr/bin/
+#mover para que sarga en el menus de app intaladas
+sudo mv calculadora.desktop /usr/share/applications
