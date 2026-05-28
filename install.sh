8c91fbfec6e953fffa72f31f9cf9f285b4f0d724
sistema200 <sistemaoperativo610@gmail.com>
2026-05-28 10:29:29 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,9 @@
 sudo chmod +x calculadora
 #mover para inicial desde la terminal
 sudo mv calculadora /usr/bin/
+#
+mkdir -p ~/.local/share/calculadora 
+sudo mv calculadora.py ~/.local/share/calculadora/
 #mover para que sarga en el menus de app intaladas
 sudo mv calculadora.desktop /usr/share/applications
 #
@@ -12,8 +15,7 @@ touch desinstalador.sh
 sudo chmod +x desinstalador.sh
 echo  "#!/bin/bash
 sudo rm -rf /usr/bin/calculadora /usr/share/applications/calculadora.desktop /usr/share/pixmaps/calculadora.png
-sudo rm -rf calculadora.py desinstalador.sh ../calculadora
-echo "Desintalado exitosamente."
-"> desinstalador.sh
+sudo rm -rf ~/.local/share/calculadora desinstalador.sh ../calculadora
+echo "Desintalado exitosamente.""> desinstalador.sh
 sudo rm -rf install.sh instruciones.txt
 echo "Instalado exitosamente."
